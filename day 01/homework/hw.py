d09612caa9addb9156832081c4869384b92eb487
ioaneboghrashvili <nanatsotskhalashvili@gmail.com>
2024-10-13 20:02:25 +0400

    first commit

---
+++ day 01/homework/hw.py
@@ -0,0 +1,6 @@
+name = "ioane"
+surname = "Boghrashvili"
+age = 9
+heigth = 125
+
+print("my name is " + name + " " + "my surname is " + surname + " " + "my age is " + str(age) + " " + "my heigth is " + str(heigth) )
---
+++ day 02/classwork/cw.py
@@ -0,0 +1,1 @@
+print("ioane boghrashvili")
